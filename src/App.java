6e96eccf3301fed1836839895594c7aaa6eb26e8
felipe soares siqueira <felipesoaressiqueira@gmail.com>
2025-04-28 12:01:15 -0300

    aula1 logica

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Alô Git!");
+        System.out.println("\t\tJEstatistics!");
+        System.out.print("-- Controle de Dados Empresarial --");
+        System.out.print("2025");
+        //ArrayList lista = new ArrayList();
     }
 }
